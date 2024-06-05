dc23b53586bb3e4f4320184679b822016828ba96
Saïd Bohui <bohuisaid@hotmail.fr>
2024-06-05 09:50:35 +0200

    add routes

--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -22,30 +22,63 @@ const productCtrl = {
             res.status(500).json({msg : error.message})
         }
     },
-
-    getProductByCategory: async(req, res) => {
-        try {
-            let products = await axios.get(`https://world.openfoodfacts.net/api/v2/search?page_size=1000&page=1`)
-            console.log("products: ", products.data)
-            res.status(200).json(products.data)
-        } catch (error) {
-            console.log("error: ", error);
-            res.status(500).json({msg : error.message})
-        }
-    },
     
     getAlternativeProducts : async(req, res) => {
         try {
             let {categories} = req.body 
             // let categories = "Beverages,Waters,Spring waters"
-            let products = await axios.get(`https://world.openfoodfacts.net/api/v2/search?&categories_tags=${categories}&page_size=100&page=1`)
+            let products = await axios.get(`https://world.openfoodfacts.net/api/v2/search?categories_tags=${categories}&page_size=100&page=1`)
             res.status(200).json(products.data)
         } catch (e) {
             console.log("e: ", e);
             res.status(500).json({msg : e.message})            
         }
+    },
+
+    getCategories: async (req, res) => {
+        let page = 1
+        let keepFetching = true
+        const arrCate = []
+        try {
+            while (keepFetching) {
+                let products = await axios.get(`https://world.openfoodfacts.org/categories.json?page_size=10000&page=${page}`)
+                console.log("products: ", products.data.tags, products.data.tags.length)
+                if (products.data.tags.length !== 0) {
+                    arrCate.push(...products.data.tags)
+                    page++
+                } else {
+                    keepFetching = false
+                }
+            }
+            res.status(200).json({ categories: arrCate })
+        } catch (error) {
+            console.log("error: ", error)
+            res.status(500).json({ msg: error.message })
+        }
+    },
+
+    getAllergens : async(req, res) => {
+        let page = 1
+        let keepFetching = true
+        const arrAllergens = []
+        try {
+            while (keepFetching) {
+                let allergens = await axios.get(`https://world.openfoodfacts.org/allergens.json?page_size=10000&page=${page}`)
+                console.log("allergens: ", allergens.data.tags, allergens.data.tags.length)
+                if (allergens.data.tags.length !== 0) {
+                    arrAllergens.push(...allergens.data.tags)
+                    page++
+                } else {
+                    keepFetching = false
+                }
+            }
+            res.status(200).json({ categories: arrAllergens })
+        } catch (error) {
+            console.log("error: ", error)
+            res.status(500).json({ msg: error.message })
+        }
     }
-};
+}
 
 
 module.exports = productCtrl;
