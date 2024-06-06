c7e6222c98b81d364f7cf894b7655bb8dd1875ad
Nathan Bertrac <nbertrac@sollan.com>
2024-06-06 15:51:45 +0200

    correction navigation

--- front/src/core/components/Navigationbar.js
+++ front/src/core/components/Navigationbar.js
@@ -1,38 +1,38 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
-import Divider from '@mui/material/Divider'
-import Drawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemText from '@mui/material/ListItemText'
-import MenuIcon from '@mui/icons-material/Menu'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import { useAuth } from '../context/authProvider'
-import logo from '../../assets/logo.svg'
-import { useNavigate } from 'react-router-dom' // Utiliser useNavigate à la place de Navigate
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useAuth } from "../context/authProvider";
+import logo from "../../assets/logo.svg";
+import { useNavigate } from "react-router-dom"; // Utiliser useNavigate à la place de Navigate
 
-const drawerWidth = 240
-const navItems = ['Home', 'About', 'Contact']
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
 
 const Navbar = (props) => {
-  const { window } = props
-  const [mobileOpen, setMobileOpen] = React.useState(false)
-  const { user, setUser } = useAuth()
-  const navigate = useNavigate() // Utiliser useNavigate ici
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const { user, setUser } = useAuth();
+  const navigate = useNavigate(); // Utiliser useNavigate ici
 
   const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState)
-  }
+    setMobileOpen((prevState) => !prevState);
+  };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Typography variant="h6" sx={{ my: 2 }}>
         MUI
       </Typography>
@@ -40,49 +40,64 @@ const Navbar = (props) => {
       <List>
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemButton sx={{ textAlign: "center" }}>
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
     </Box>
-  )
+  );
 
   const logout = async () => {
     try {
-      setUser(null)
-      localStorage.removeItem('user')
-      navigate('/') 
+      setUser(null);
+      localStorage.removeItem("user");
+      navigate("/");
     } catch (error) {
-      console.log(error)
-      return 'impossible de vous déconnecter'
+      console.log(error);
+      return "impossible de vous déconnecter";
     }
-  }
+  };
 
-  const container = window !== undefined ? () => window().document.body : undefined
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{ backgroundColor: '#DF921A' }}>
+      <AppBar component="nav" sx={{ backgroundColor: "#DF921A" }}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
+            sx={{ mr: 2, display: { sm: "none" } }}>
             <MenuIcon />
           </IconButton>
           <Box sx={{ flexGrow: 1 }}>
-            <img src={logo} alt="logo" style={{ height: '40px', marginRight: '16px' }} />
+            <img
+              src={logo}
+              alt="logo"
+              style={{ height: "40px", marginRight: "16px" }}
+            />
           </Box>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <Button sx={{ color: '#fff' }}>Produits</Button>
-            {user ? <Button sx={{ color: '#fff' }} onClick={logout}>Déconnexion</Button> :
-            <Button sx={{ color: '#fff' }} onClick={() => navigate('/authentication')}>Connexion</Button>}
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Button sx={{ color: "#fff" }} onClick={() => navigate("/")}>
+              Produits
+            </Button>
+            {user ? (
+              <Button sx={{ color: "#fff" }} onClick={logout}>
+                Déconnexion
+              </Button>
+            ) : (
+              <Button
+                sx={{ color: "#fff" }}
+                onClick={() => navigate("/authentication")}>
+                Connexion
+              </Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
@@ -96,19 +111,21 @@ const Navbar = (props) => {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}>
           {drawer}
         </Drawer>
       </nav>
     </Box>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   window: PropTypes.func,
-}
+};
 
-export default Navbar
+export default Navbar;
